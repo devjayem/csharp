2457d149cac82b0863252288a0e9e6933f0d27b9
John Mark Eugenio <82132828+devjayem@users.noreply.github.com>
2023-10-05 16:54:16 +0800

    Finish Exercism C# Track

---
+++ roll-the-die/RollTheDie.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Player
+{
+    Random rnd = new Random();
+    public int RollDie()
+    {
+        return rnd.Next(1, 18);
+    }
+
+    public double GenerateSpellStrength()
+    {
+        return rnd.NextDouble();
+    }
+}
