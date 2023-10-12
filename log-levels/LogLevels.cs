9927061f9cac195d63c2f7f47cd0139896a96683
John Mark Eugenio <82132828+devjayem@users.noreply.github.com>
2023-10-12 15:36:34 +0800

    Finished exercism c# track

--- log-levels/LogLevels.cs
+++ log-levels/LogLevels.cs
@@ -2,18 +2,12 @@ using System;
 
 static class LogLine
 {
-    public static string Message(string logLine)
-    {
-        throw new NotImplementedException("Please implement the (static) LogLine.Message() method");
-    }
+    public static string Message(string logLine) =>
+        logLine[(logLine.IndexOf(':') + 1)..].Trim();
 
-    public static string LogLevel(string logLine)
-    {
-        throw new NotImplementedException("Please implement the (static) LogLine.LogLevel() method");
-    }
+    public static string LogLevel(string logLine) =>
+        logLine[1..logLine.IndexOf(']')].ToLower();
 
-    public static string Reformat(string logLine)
-    {
-        throw new NotImplementedException("Please implement the (static) LogLine.Reformat() method");
-    }
+    public static string Reformat(string logLine) =>
+        $"{Message(logLine)} ({LogLevel(logLine)})";
 }
